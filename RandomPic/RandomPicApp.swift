c222741a1b3426cbe2682d5845025d3ed9a38f2c
rong1002 <borong1002@gmail.com>
2020-12-16 13:00:40 +0800

    Initial Commit

---
+++ RandomPic/RandomPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPicApp.swift
+//  RandomPic
+//
+//  Created by Lin Bo Rong on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct RandomPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
