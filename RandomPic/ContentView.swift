b2b8f9db31ea78e5267fef2c858a9332db73df12
rong1002 <borong1002@gmail.com>
2021-01-20 01:21:37 +0800

    updata

--- RandomPic/ContentView.swift
+++ RandomPic/ContentView.swift
@@ -6,47 +6,72 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    
-    var urlString: String
-    @State private var Pic = Image(systemName: "photo")
-    @State private var ChangePic = false
-    func Change(){
-        if let url = URL(string: urlString),
-           let data = try? Data(contentsOf: url),
-           let uiImage = UIImage(data: data){
-            Pic = Image(uiImage: uiImage)
-            ChangePic = true
-        }
-    }
-    var body: some View{
-        Pic.resizable()
-            .onAppear{
-                if ChangePic == false {
-                    Change()
-                }
-            }
-        Button(action: {
-            Change()
-        }) {
-            HStack {
-                Image(systemName: "magnifyingglass.circle.fill")
-                Text("Change Picture")
-                Image(systemName: "magnifyingglass.circle.fill")
-            }
-            .font(.system(size: 20))
-            .foregroundColor(.black)
-            .padding()
-            .background(LinearGradient(gradient: Gradient(colors: [Color.init(red: 120/255, green: 113/255, blue: 204/255), Color.init(red: 218/255, green: 217/255, blue: 255/255)]), startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 1, y: 1)))
-            .cornerRadius(20)
-            .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 5))
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(urlString: "https://picsum.photos/200/300")
+struct ContentView: View {
+    @ObservedObject var imagePicker: ImagePicker
+    @State var image:UIImage = UIImage()
+    
+    init() {
+        imagePicker = ImagePicker(urlString: "https://picsum.photos/200/300")
     }
+    var body: some View {
+        
+        VStack{
+            Image(uiImage: imagePicker.data != nil ? UIImage(data: imagePicker.data!)!: UIImage())
+                .resizable()
+                .scaledToFit()
+            Button(action:{
+                imagePicker.Change()
+            }){
+                HStack {
+                    Image(systemName: "magnifyingglass.circle.fill")
+                    Text("Change Picture")
+                    Image(systemName: "magnifyingglass.circle.fill")
+                }
+                .font(.system(size: 20))
+                .foregroundColor(.black)
+                .padding()
+                .background(LinearGradient(gradient: Gradient(colors: [Color.init(red: 120/255, green: 113/255, blue: 204/255), Color.init(red: 218/255, green: 217/255, blue: 255/255)]), startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 1, y: 1)))
+                .cornerRadius(20)
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 5))
+            }
+        }
+        
+    }
+}
+
+class ImagePicker: ObservableObject {
+    @Published var data:Data?
+    
+    var urlString: String = "https://picsum.photos/200/300"
+    @State var change:Int?
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+    
+    func Change()
+    {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            DispatchQueue.main.async{
+                self.data = data
+                
+            }
+        }
+        task.resume()
+    }
+    
 }
--- RandomPic/RandomPicApp.swift
+++ RandomPic/RandomPicApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RandomPicApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(urlString: "https://picsum.photos/200/300")
+            ContentView()
         }
     }
 }
